8a5371a3bc2f9a4f21506de7500c64e26b4c9c74
yqh231 <442621753@qq.com>
2017-04-29 18:28:12 +0800

    dev

--- blog/mylib.py
+++ blog/mylib.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
-from blog.blogDB import get_db
+import datetime
+
+from blogDB import Session
+from model import tbl_blog,tbl_comm,tbl_tag
 
 #定义类
 class password:
@@ -21,50 +24,77 @@ class password:
 class Article:
     def __init__(self, id):
         self.id = id
+        self.session = Session()
     def getExit(self):
         blogdb = get_db()
-        cur = blogdb.cursor()
+        cur = blogdb
         cur.execute('SELECT id, title, abstract, tag, date, file FROM blog where id = ?',(self.id,))
         self.exit = cur.fetchall()[0]
         return self.exit
     def getArti(self):
         blogdb = get_db()
-        cur = blogdb.cursor()
+        cur = blogdb
         cur.execute('SELECT title, date, content, tag, abstract from blog where id = ?',(self.id,))
         self.arti = cur.fetchall()[0]
         return self.arti
     def getEdit(self):
         blogdb = get_db()
-        cur = blogdb.cursor()
+        cur = blogdb
         cur.execute('SELECT title, content,tag from blog where id = ?',(self.id,))
         content = cur.fetchall()[0]
         self.title = content[0]
         self.content = content[1]
         self.tag = content[2]
+
     def update(self, title, tag, img, file, content):
         abstract = abstr(content,img)
         tags = (tag or '').replace('，',',')
-        blogdb = get_db()
-        cur = blogdb.cursor()
         if self.id:
-            cur.execute('UPDATE blog SET title = ? ,content = ?,abstract = ?,tag = ? ,file = ? WHERE ID = ?;', (title, content,abstract,tags,file, self.id))
+            blog = self.session.query(tbl_blog).filter(tbl_blog.id == self.id).first()
+            blog.title = title
+            blog.content = content
+            blog.abstract = abstract
+            blog.tag = tags
+            blog.file = file
+
+            self.session.commit()
+
         else:
-            cur.execute('insert into blog (title,tag,file,abstract,content) values (?, ?, ?, ?, ?)', (title,tags,file,abstract,content))
-            cur.execute('select id from blog order by id desc limit 1')
-            blog = cur.fetchall()
-            self.id = blog[0][0]
-        blogdb.commit()
-        cur.execute('delete from tag where blog = ?',(self.id,))
+            new_blog = tbl_blog(
+                title = title,
+                content = content,
+                abstract = abstract,
+                date = datetime.datetime.now(),
+                file = file
+            )
+            self.session.add(new_blog)
+            self.session.commit()
+
+            query_result = self.session.query(tbl_blog.id).order_by(tbl_blog.id.desc()).first()
+            self.id = query_result.id
+
+        #delete old tag data
+        query_result = self.session.query(tbl_tag).filter(tbl_tag.blog == self.id)
+        query_result.delete()
+
         if tags:
             tags = tags.split(',')
         else:
             tags = []
+
         for tag in tags:
-            cur.execute('insert into tag (tag, blog) values (?, ?)', (tag, self.id))
-        blogdb.commit()
+            new_tag = tbl_tag(
+                            tag = tag,
+                            blog = self.id
+                                )
+            self.session.add(new_tag)
+        self.session.commit()
+
+
+
     def delArti(self):
         blogdb = get_db()
-        cur = blogdb.cursor()
+        cur = blogdb
         cur.execute('DELETE FROM blog WHERE id = ? ',(self.id,))
         cur.execute('DELETE FROM tag WHERE blog = ? ',(self.id,))
         cur.execute('DELETE FROM comm WHERE blog = ? ',(self.id,))
@@ -73,10 +103,11 @@ class Article:
 class comment:
     def __init__(self, id = 0 ):
         self.id = id
+        self.session = Session()
     def commList(self):
         try:
             blogdb = get_db()
-            cur = blogdb.cursor()
+            cur = blogdb
             cur.execute(' SELECT content, date, author, id, reply FROM comm WHERE blog = ? ORDER BY id DESC',(self.id,))
             temp = cur.fetchall() or []
             temp = list(temp)
@@ -100,22 +131,26 @@ class comment:
         finally:
             return self.cList
     def getNew(self):
-        blogdb = get_db()
-        cur = blogdb.cursor()
-        cur.execute(' SELECT content, date, author, id, blog FROM comm ORDER BY id DESC LIMIT 12')
-        temp = cur.fetchall() or []
+        query_para = (
+            tbl_comm.author,
+            tbl_comm.id,
+            tbl_comm.blog
+        )
+        query_result = self.session.query(*query_para).order_by(tbl_comm.id.desc())
+        temp = query_result.all() or []
         self.cList = temp
         return self.cList
+
     def insert(self, content, author, reply):
         author = author or u'访客'
         reply = reply or None
         blogdb = get_db()
-        cur = blogdb.cursor()
+        cur = blogdb
         cur.execute('insert into comm (content, author, blog, reply) values (?, ?, ?, ?)', (content, author, self.id, reply))
         blogdb.commit()
     def dele(self):
         blogdb = get_db()
-        cur = blogdb.cursor()
+        cur = blogdb
         cur.execute('DELETE FROM comm WHERE id = ? ',(self.id,))
         blogdb.commit()
 
@@ -124,6 +159,7 @@ class artiList:
         self.method = method
         self.key = key
         self.page = ( page - 1 ) * 8
+        self.session = Session()
     def getAl(self):
         result = []
         for arti in self.al:
@@ -134,7 +170,7 @@ class artiList:
         return self.result
     def getLen(self):
         blogdb = get_db()
-        cur = blogdb.cursor()
+        cur = blogdb
         if self.method == 'file':
             cur.execute('SELECT count(*) FROM blog WHERE file = ?;',(self.key,))
         elif self.method == 'tag':
@@ -147,7 +183,7 @@ class artiList:
         return self.len
     def alUpdate(self):
         blogdb = get_db()
-        cur = blogdb.cursor()
+        cur = blogdb
         if self.method == 'file':
             cur.execute(' SELECT id FROM blog WHERE file = ? ORDER BY id DESC LIMIT 8 OFFSET ?',(self.key,self.page,))
         elif self.method == 'tag':
@@ -180,30 +216,8 @@ def abstr(text,img = ""):
     return text
 
 def exper1():
-    cur=get_db().cursor()
+    cur=get_db()
     cur.execute('''SELECT TAG, COUNT(*) FROM TAG GROUP BY TAG ORDER BY ID DESC;''')
     print cur.fetchall()
 
 
-def inita():
-    cur=get_db().cursor()
-    cur.execute('''CREATE TABLE blog
-       (ID         SERIAL    PRIMARY KEY,
-       title       TEXT,
-       content     TEXT      NOT NULL,
-       abstract    TEXT      NOT NULL,
-       date        TIMESTAMP DEFAULT (CURRENT_TIMESTAMP(0) + interval '1 hour'), 
-	   tag         TEXT,
-	   file        INT)''')
-    cur.execute('''CREATE TABLE tag
-       (ID         SERIAL    PRIMARY KEY,
-	    tag        TEXT,
-	    blog       INT)''')
-    cur.execute('''CREATE TABLE comm
-       (ID         SERIAL    PRIMARY KEY,
-       author      TEXT,
-       content     TEXT      NOT NULL,
-       blog        INT       NOT NULL,
-       date        TIMESTAMP DEFAULT (CURRENT_TIMESTAMP(0) + interval '8 hour'), 
-       reply       smallint)''')
-    get_db().commit()
