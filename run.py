0d50b27a384901b5315f99f339a5e0dfb6df2b76
yqh231 <442621753@qq.com>
2017-05-02 15:34:04 +0800

    fix bug

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from blog import app
 from blog.blogDB import config_db_session
 from werkzeug.contrib.fixers import ProxyFix
 config_db_session()
-app.wsgi_app = ProxyFix(app.wsgi_app)
+# app.wsgi_app = ProxyFix(app.wsgi_app)
+app.run()
